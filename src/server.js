154ea9bca98d27eedcd3a778de24c2e9eaba2f08
haneen <haneen.khasawneh96@gmail.com>
2021-04-04 15:22:27 +0300

    test

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 'use strict';
 const express = require('express');
-
-
+const morgan = require('morgan');
+const cors = require('cors');
 // require('dotenv').config();
 
-
+const clothRouter = require('./routes/clothes.js');
+const foodRouter = require('./routes/food.js');
 
 const notFoundHndler = require('./error-handlers/404.js');
 const errorHandler = require('./error-handlers/500.js');
@@ -12,9 +13,10 @@ const errorHandler = require('./error-handlers/500.js');
 const app = express();
 
 app.use(express.json());
-
-
-
+app.use(morgan('dev'));
+app.use(cors());
+app.use('/api/v1/cloth/', clothRouter);
+app.use('/api/v1/food/', foodRouter);
 
 function homeHandler(req, res) {
     res.send('Hello World');
